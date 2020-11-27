bb7d503a5725042844f4b2990faa200d50bc63fc
OlaLe <lepkowska.ola@gmail.com>
2020-11-27 13:41:29 +0100

    settings.py changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a&c9x3rit_+%6ieolvm+b889cnb)44505y&5$-1us#i*t5(-uc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://mysite.pythonanywhere.com', 'alelep1.pythonanywhere.com']
 
 
 # Application definition
